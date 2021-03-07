8ef1bcb3c0e9ab02d2f5b4e0cee4b13796cdfbdf
Ronald Kunenborg <ronaldkunenborg@grundsatzlich-it.nl>
2021-03-07 19:02:50 +0100

    Improved example.py to show more metadata. Added test_pywave.py for pytest, this tests the metadata of the sample file "file.wav".

--- example.py
+++ example.py
@@ -1,16 +1,40 @@
 import PyWave
 
+
+# I will try to integrate this in the next iteration of PyWave
+def get_format_name(waveformat):
+    waveformatnames_dict = {
+        0x0000: 'Unknown Wave Format',
+        0x0001: 'WAVE_FORMAT_PCM',
+        0x0002: 'WAVE_FORMAT_ADPCM',
+        0x0003: 'WAVE_FORMAT_IEEE_FLOAT',
+        0x0006: 'WAVE_FORMAT_ALAW',
+        0x0007: 'WAVE_FORMAT_MULAW',
+        0xFFFE: 'WAVE_FORMAT_EXTENSIBLE'
+    }
+    return waveformatnames_dict.get(waveformat, 'Unknown')
+
+
 PATH = "path/to/a/wave/file.wav"
+PATH_COPY = "path/to/a/wave/file_copy.wav"
 
 wf = PyWave.open(PATH)
 
-print("This WAVE file has the following properties:")
+print("File '{0}' was opened succesfully.".format(PATH))
+print("This WAVE file has the following properties:\n")
+
+print("format code:", wf.format, "({0})\n".format(get_format_name(wf.format)))
+
 print(wf.channels, "channels")
 print(wf.frequency, "Hz sample rate")
-print(wf.bitrate, "bits per second")
-print(wf.samples, "total samples")
+print(wf.bits_per_sample, "bits ({0} bytes) per sample".format(wf.bytes_per_sample))
+print(wf.bitrate, "bits ({0} bytes) per second".format(wf.average_bytes_per_sec))
 
-wf_copy = PyWave.open("path/to/a/wave/file_copy.wav", 
+print(wf.samples, "total samples")
+print(wf.data_length, "bytes of data")
+print(wf.block_align, "bytes per block (1 sample for each channel)")
+
+wf_copy = PyWave.open(PATH_COPY, 
                       mode = "w",
                       channels = wf.channels,
                       frequency = wf.frequency,
@@ -19,6 +43,8 @@ wf_copy = PyWave.open("path/to/a/wave/file_copy.wav",
 wf_copy.write(wf.read())
 wf.close()
 wf_copy.close()
+
+print("\nA copy of the wav-file was created as: '{0}'".format(PATH_COPY))
 
 if len(wf.messages) > 0:
     print ('\nAll warnings during processing:\n-------------------------------')
--- test_pywave.py
+++ test_pywave.py
@@ -16,14 +16,29 @@ def test_read(wf):
     assert isinstance(wf, object)
 
     # due to the read(1) we should have a warning
+    assert wf.data_position == 8            # 32bit float, 2 channels = 8 bytes minimum read for 1 sample
     assert (len(wf.messages) > 0)
 
     assert wfile is not None
-    assert len(wfile) == wf.block_align           # make sure we read on BlockAlignments
+    assert len(wfile) == wf.block_align     # make sure we read on BlockAlignments
 
 
 def test_metadata(wf):
+    assert wf.format == 0x0003              # WAVE_FORMAT_IEEE_FLOAT
     assert wf.channels == 2
     assert wf.frequency == 44100
+    assert wf.samples_per_sec == wf.frequency
     assert wf.bitrate == 2822400
+    assert wf.bitrate == wf.average_bytes_per_sec * 8
+
+    assert wf.bits_per_sample == 32         # 1 sample = 32 bit float
+    assert wf.bits_per_sample == wf.bytes_per_sample * 8
+    assert wf.block_align == wf.bits_per_sample * wf.channels  # 32bit float, 2 channels = 8 bytes minimum read for 1 sample 
+
+    assert wf.data_length == wf.samples * wf.channels * wf.bytes_per_sample # 99328 * 2 channels * 4 bytes per sample
     assert wf.samples == 99328
+    assert wf.samples == (wf.data_length // wf.bytes_per_sample // wf.channels)
+
+    assert wf.data_starts_at == 88
+    assert wf.data_length == ( wf.end_of_data - wf.data_starts_at )
+
