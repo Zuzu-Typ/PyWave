13b5b33d24590ef62228cf441e466e2eabbc4875
Ronald Kunenborg <ronaldkunenborg@grundsatzlich-it.nl>
2021-03-14 08:28:22 +0100

    Several changes for added robustness (and more) (#3)
    * Several changes for robustness:
    - can now have warnings in list for later processing, especially for the cases fixed in this release
    - can handle incorrect filesize in metadata
    - aligns read with block_align size
    - can handle misaligned chunks
    - can handle misaligned LIST form type ID due to null-terminated "LIST" tag
    - no longer crashes in prepare_read when trying to raise an error because path is undefined
    - added reporting of warnings to example.py
    
    * Added more documentation
    
    Added a little bit of doc to the functions.
    Also fixed a missing comma in line 114 (now line 376)
    
    * test can now also check for messages when reading 1 sample (less than blockalign)added a pytest file, which tests the example wav-file. Install pytest beforehand.
    
    * Improved example.py to show more metadata. Added test_pywave.py for pytest, this tests the metadata of the sample file "file.wav".
    
    * small error fixed in assert of blockalign in pytest
    
    * Added a note on PyTest in the README, as well as a requirements.txt file for easy installation of pytest.
    
    * Added all the stuff I had, which includes: adding fourCC codes for a lot of chunks, parsing disp, bext, cart and peak chunks, reading all other chunks as bytestring in the metadata, adding support for metadata on speakers and channel layout, small bugfixes for invalid filename incorrect __del__, a lot more support for WAVE_FORMAT_EXTENSIBLE metadata, etc.
    
    Updated the pytest file as well, but is not yet complete in coverage.
    
    * Small changes
    
    + Added `format_name` property
    - Removed old readme files.
    
    Co-authored-by: Zuzu-Typ <Zuzu.Typ@gmail.com>

---
+++ test_pywave.py
@@ -0,0 +1,64 @@
+import PyWave
+import pytest
+
+
+@pytest.fixture
+def wf():
+    PATH = "path/to/a/wave/file.wav"
+    wavefile = PyWave.open(PATH)
+    yield wavefile
+    wavefile.close()
+
+
+def test_read(wf):
+    wfile = wf.read(1)
+    assert wf is not None
+    assert isinstance(wf, object)
+
+    # due to the read(1) we should have a warning
+    assert wf.data_position == 8            # 32bit float, 2 channels = 8 bytes minimum read for 1 sample
+    assert (len(wf.messages) > 0)
+
+    assert wfile is not None
+    assert len(wfile) == wf.block_align     # make sure we read on BlockAlignments
+
+
+def test_metadata(wf):
+    assert wf.format == 0x0003              # WAVE_FORMAT_IEEE_FLOAT
+    assert wf.channels == 2
+    assert wf.frequency == 44100
+    assert wf.samples_per_sec == wf.frequency
+    assert wf.bitrate == 2822400
+    assert wf.bitrate == wf.average_bytes_per_sec * 8
+
+    assert wf.bits_per_sample == 32         # 1 sample = 32 bit float
+    assert wf.bits_per_sample == wf.bytes_per_sample * 8
+    assert wf.block_align == wf.bytes_per_sample * wf.channels  # 32bit float = 4 bytes * 2 channels = 8 bytes minimum read for 1 sample 
+
+    assert wf.data_length == wf.samples * wf.channels * wf.bytes_per_sample # 99328 * 2 channels * 4 bytes per sample
+    assert wf.samples == 99328
+    assert wf.samples == (wf.data_length // wf.bytes_per_sample // wf.channels)
+
+    assert wf.data_starts_at == 88
+    assert wf.data_length == ( wf.end_of_data - wf.data_starts_at )
+
+    waveformatcode, waveformatname = wf.format_name
+    assert waveformatcode == 'WAVE_FORMAT_IEEE_FLOAT'
+    assert waveformatname == 'IEEE Float'
+
+
+def test_static_methods(wf):
+    assert wf.get_format_name(wf.format) == wf.format_name
+
+    assert wf.get_channel_layout(0b111111,6) == ['Front Left', 'Front Right', 'Front Center', 'Low Frequency', 'Back Left (Surround Back Left)', 'Back Right (Surround Back Right)']
+    assert wf.get_channel_setup_name(0b111111, 6) == '5.1'
+
+
+# test if the __del__ on the class does not fail when the file cannot be opened and __init__ fails
+# also test whether we get the right exception back
+#
+# an Attribute Error in PyWave.__del__ indicates failure to correctly initialize variables before opening a wavefile.
+# a FileNotFoundError is the expected outcome.
+def test_delete():
+    with pytest.raises(FileNotFoundError):
+        wavefile = PyWave.open("xxxx.yyy")
